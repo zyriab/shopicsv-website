bb43b0d3bbcbcb106ad6aab753be3b63b3795d72
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-20 20:54:37 +0200

    feature: trying to add a favicon (Gatsby is hell)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,29 @@
-import type { GatsbyConfig } from "gatsby"
-import dotenv from 'dotenv'
+import type { GatsbyConfig } from 'gatsby';
+import dotenv from 'dotenv';
 
 dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`
-})
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.shopicsv.app/`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'ShopiCSV',
+        short_name: 'ShopiCSV',
+        start_url: '/',
+        background_color: '#f6f6f7',
+        theme_color: '#178b6e',
+        display: 'standalone',
+        icon: 'src/images/favicon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+};
 
-export default config
+export default config;
