965ba9e4e3a418a97b2d38f729541a5495dd45fc
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-30 14:28:43 +0100

    chore: added site metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,13 +7,16 @@ dotenv.config({
 
 const config: GatsbyConfig = {
   siteMetadata: {
+    title: `ShopiCSV - Time to ditch Excel`,
+    description: `An awesome and non-intrusive business-oriented SaaS especially crafted to edit Shopify's CSV files without destroying your shop while maximizing your productivity`,
+    image: `/logo.svg`,
     siteUrl: `https://www.shopicsv.app/`,
   },
   plugins: [
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'ShopiCSV',
+        name: 'ShopiCSV - Time to ditch Excel',
         short_name: 'ShopiCSV',
         start_url: '/',
         background_color: '#f6f6f7',
