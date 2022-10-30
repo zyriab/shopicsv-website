0cdbfbf14c68d9b12d27386cf74bc6b7dafa39ce
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-30 15:10:12 +0100

    fix: fixing bad metadata

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -22,22 +22,29 @@ export default function Seo(props: SeoProps) {
     description: props.description || defaultDescription,
     image: `${siteUrl}${image}`,
     url: `${siteUrl}${props.pathname || ``}`,
-    twitterUsername,
-  }
+  };
 
   return (
     <>
       <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+      <meta name="title" property="og:title" content={seo.title}></meta>
+      <meta
+        name="description"
+        property="og:description"
+        content={seo.description}
+      />
+      <meta name="image" property="og:image" content={seo.image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
-      <meta name="twitter:creator" content={seo.twitterUsername} />
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" />
+      {/* <meta name="twitter:creator" content={seo.twitterUsername} /> */}
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
+      />
       {props.children}
     </>
-  )
+  );
 }
